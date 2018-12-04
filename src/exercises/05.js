7fee567ad704c6bfc23e38c469accb4ec49f5b46
Daniyar <ddr199305@gmail.com>
2018-12-04 12:26:49 +0100

    Ex.5. Apply useRef && useLayotEffect hooks

--- src/exercises/05.js
+++ src/exercises/05.js
@@ -1,37 +1,25 @@
-// VanillaTilt: useRef
-// 🐨 1. you'll want useRef, and useLayoutEffect
-// you can use `useLayoutEffect` the same way you use `useEffect`.
-// Make sure to ask me what the difference is!
-// https://reactjs.org/docs/hooks-reference.html#useref
-// https://reactjs.org/docs/hooks-reference.html#uselayouteffect
-import React from 'react'
-// 🐨 2. you'll need this:
-// import VanillaTilt from 'vanilla-tilt'
+import React, {useRef, useLayoutEffect} from 'react'
+import VanillaTilt from 'vanilla-tilt'
 
 function Tilt(props) {
-  // 🐨 3. create a `tiltNode` variable here with `useRef()`
-  // 🐨 5. create a `useLayoutEffect` callback here which
-  //    uses the `VanillaTilt.init` with `tiltNode.current`
-  // 🐨 6: you'll need this in your callback:
-  // const vanillaTiltOptions = {
-  //   max: 25,
-  //   speed: 400,
-  //   glare: true,
-  //   'max-glare': 0.5,
-  // }
-  // 🐨 7. return a cleanup function which will call
-  //   `tiltNode.current.vanillaTilt.destroy()`
+  // create ref
+  const tiltNode = useRef()
 
-  // By default, effects run after every render. This is normally what
-  // you want, but if you want you can optimize things by ensuring they
-  // are only called when you specifically want to.
-  // 💯 add a second argument to your `useLayoutEffect` which lists an empty
-  // array. We can do this because we only want it to run on initial render
-  // and we know that the `tiltRef.current` will never change.
+  // Signature is identical to useEffect (async),
+  // but fires synchronously after all DOM mutations
+  useLayoutEffect(() => {
+    const {current} = tiltNode
+    VanillaTilt.init(current, {
+      max: 25,
+      speed: 400,
+      glare: true,
+      'max-glare': 0.5,
+    })
+    return () => current.vanillaTilt.destroy()
+  }, [])
 
-  // 🐨 4. pass the `tiltNode` variable to this `div` as the `ref` prop:
   return (
-    <div className="tilt-root">
+    <div className="tilt-root" ref={tiltNode}>
       <div className="tilt-child">{props.children}</div>
     </div>
   )
